9c7329279c2c824eaac1cf67f3eeae4afc2a8b1d
Latteflo <sf.simion.f@gmail.com>
2022-06-03 21:56:14 +0200

    fixed animation on letter F

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layouts';
+import Layout from './/components/Layouts';
 import Home from './components/Home';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route index element={<Home />} />
-
+      <Route path="/" element={< Layout />} />
+      <Route index element={< Home />} >
+      </Route>
       </Routes>
       </>
   );
