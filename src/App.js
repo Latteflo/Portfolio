b45c880528addfa9c556b0f6433d9b57e6d597e4
Latteflo <sf.simion.f@gmail.com>
2022-06-03 21:45:06 +0200

    Created Home page and animated the first letter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layouts';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<Layout />} />
+      <Route index element={<Home />} />
+
       </Routes>
       </>
   );
