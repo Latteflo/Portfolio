a9b15d73e5a0dbb3ec3f5b4e486eef409be38f41
Latteflo <sf.simion.f@gmail.com>
2022-06-07 00:00:59 +0200

    changed letterClass function

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,30 +10,37 @@ const [letterClass, setLetterClass]= useState('text-animate')
 const nameArray=['l','o', 'r','e', 'n', 't', 'i', 'n', 'a', ' ', 'S', 'i', 'm', 'i', 'o', 'n']
 const jobArray=['W', 'e', 'b',' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r','.']
 
-useEffect (()=>{
-    return setTimeout(()=>{
-        setLetterClass('text-animate-hover')
+useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
     }, 4000)
-},[])
+  }, [])
 
     return(
         <div className="container home-page">
             <div className="text-zone">
             <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass}_12`}>i</span>
-                <br/> 
-                <span className={`${letterClass}_13`}>I</span>
-                <span className={`${letterClass}_14`}>'m</span>
-            <img src={LogoTitle} alt="developer"/>
-            <AnimatedLetters letterClass={letterClass} 
-            strArray={nameArray} 
-            idx={15}/>
-            <br/>
-            <AnimatedLetters letterClass={letterClass} 
-            strArray={jobArray} 
-            idx={22}/>
-            </h1>    
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
             <h2>Frontend Developer  / JavaScript Enthusiast / FullStack Aspirrer</h2>
             <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
