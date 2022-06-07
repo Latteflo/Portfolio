a37df8e7a7c780d44a1dded8e10c8d76fcb25a28
Latteflo <sf.simion.f@gmail.com>
2022-06-07 22:23:44 +0200

    fixed h1 animation bug and style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import LogoTitle from '../../assets/images/logo-f.png'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import'./index.scss'
+import LogoTitle from '../../assets/images/logo-f.png'
+import './index.scss'
 
 
 const Home =()=>{
@@ -11,41 +12,47 @@ const nameArray=['l','o', 'r','e', 'n', 't', 'i', 'n', 'a', ' ', 'S', 'i', 'm', 
 const jobArray=['W', 'e', 'b',' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r','.']
 
 useEffect(() => {
-    return setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-  }, [])
+  setTimeout(() => {
+    setLetterClass('text-animate-hover')
+  }, 4000);
+}, []);
 
-    return(
-        <div className="container home-page">
-            <div className="text-zone">
-            <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
-          </h1>
-            <h2>Frontend Developer  / JavaScript Enthusiast / FullStack Aspirrer</h2>
-            <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-        </div>
-    )
+return (
+  <>
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img
+            src={LogoTitle}
+            alt="JavaScript Developer Name, Web Developer Name"
+          />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer  / JavaScript Enthusiast / FullStack Aspirrer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+
+    <Loader type="pacman" />
+  </>
+)
 }
 
 export default Home
