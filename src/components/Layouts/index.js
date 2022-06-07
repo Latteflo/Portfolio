72bc847dc95d17a3f7b0c4bfada50228cc6be41a
Latteflo <sf.simion.f@gmail.com>
2022-06-07 23:56:12 +0200

    added <p> tags to the layout of about section

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -7,6 +7,8 @@ const Layout =()=>{
     <div className='App'>
         <Sidebar/>
         <div className='page'>
+        <span className="top-tag-html">
+            &lt;html&gt;</span>
         <span className="tags top-tags">
             &lt;body&gt;</span>
                 <Outlet/>
