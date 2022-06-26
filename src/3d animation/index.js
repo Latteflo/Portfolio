d1effc7fcba533a14ff5c1896a6ade1edad67edf
Latteflo <sf.simion.f@gmail.com>
2022-06-26 12:05:56 +0200

    added title to iframe

--- src/3d animation/index.js
+++ src/3d animation/index.js
@@ -1,9 +1,10 @@
-import react from "react";
+import React from "react";
 
 function Blob(){
     return (
+
         <iframe src='https://my.spline.design/interactivespherescopy-72f927998eac79970d2e8dec0599d9e0/'
-         frameborder='0' width='100%' height='100%'>
+         frameborder='0' width='100%' height='100%' title="rotationg ball of light animated in exterior with base colours">
 
         </iframe>
     )
