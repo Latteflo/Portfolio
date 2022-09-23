cfcdfe5f47aa3473aabad5b4ab1e119153db2d43
Latteflo <sf.simion.f@gmail.com>
2022-09-23 12:23:17 +0200

    updated about me paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -29,6 +29,7 @@ const About = () => {
            <br/>
         <p> I am an ambitious front end developer based in Belgium, currently looking for a role in an IT company with the opportunity to work with the latest technologies on diverse projects. </p>
         <p>I'm quietly confident, naturally curious and perpetually working on improving my skills.</p>
+        <p>With a background in aviation, my experience has given me the ability to problem solve and think outside the box, which has come in handy in my career as a developer. I have also found that my attention to detail is an asset when working on web projects.</p>
         <p>I'm a fast learner and I'm always looking for new challenges and opportunities to grow as a developer.</p>
         </div>
         <WordCloud/>
