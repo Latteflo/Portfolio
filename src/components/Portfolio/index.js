e0c49612e6bb5e548a0f4eeea2c470188927a043
Latteflo <sf.simion.f@gmail.com>
2022-11-28 22:34:53 +0100

    added carousel slider

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
 import portfolioData from '../../data/portfolio.json'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import './index.scss'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -19,30 +24,52 @@ const Portfolio = () => {
 
   const renderPortfolio = (portfolio) => {
     return (
-      <div className="images-container">
-        {portfolio.map((port, idx) => {
-          return (
-            <div className="image-box" key={idx}>
-              <img
-                src={port.cover}
-                className="portfolio-image"
-                alt="portfolio"
-              />
-
-              <div className="content">
-                <p className="title"> {port.title} </p>
-                <p className="technologies">
-                  <span>Build with:</span> {port.technologies}{' '}
-                </p>
-                <h4 className="description"> {port.description} </h4>
-                <button className="btn" onClick={() => window.open(port.url)}>
-                  View
-                </button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      <>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={440}
+          slidesPerGroup={1}
+          centeredSlides={true}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          
+            {portfolio.map((port, idx) => {
+              return (  
+                <div className="slide" key={idx}>
+                  <SwiperSlide>  
+                    <div className="portfolio-item">
+                    <img
+                    src={port.cover}
+                    className="portfolio-image"
+                    alt="portfolio"
+                  />
+                  <div className="content">
+                    <p className="title"> {port.title} </p>
+                    <p className="technologies">
+                      <span>Build with:</span> {port.technologies}{' '}
+                    </p>
+                    <h4 className="description"> {port.description} </h4>
+                    <button
+                      className="btn"
+                      onClick={() => window.open(port.url)}
+                    >
+                      View
+                    </button>
+                  </div>    
+                  </div>
+                  </SwiperSlide>
+                </div>    
+              )
+            })}  
+        </Swiper>
+      </>
     )
   }
 
@@ -61,7 +88,7 @@ const Portfolio = () => {
         </div>
         <div className="portfolio-data">
           {renderPortfolio(portfolioData.portfolio)}
-          </div>
+        </div>
       </div>
     </>
   )
