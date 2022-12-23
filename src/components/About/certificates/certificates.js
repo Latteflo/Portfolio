32d4b5821d5e57850f19b367883bfab2868c06de
Latteflo <sf.simion.f@gmail.com>
2022-12-23 01:43:41 +0100

    Create certificates.js

---
+++ src/components/About/certificates/certificates.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './certificates.scss'
+
+const Certificates = () => {
+    return (
+        <div>
+        <h1>This page is under construction.</h1>
+        <h2>Check back soon!</h2>
+        </div>
+    );
+ }
+
+export default Certificates;
