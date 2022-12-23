07aa5c295b25387516b67a62cf7ae3d249cce513
Latteflo <sf.simion.f@gmail.com>
2022-12-23 01:43:49 +0100

    Create resume.js

---
+++ src/components/About/resume/resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './resume.scss';
+
+const Resume = () => {
+  return (
+    <div className="resume">
+<h1>This page is under construction!</h1>
+<h2>Check back soon!</h2>
+    </div>
+  );
+};
+
+export default Resume;
+
+
