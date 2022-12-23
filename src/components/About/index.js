fc1587975c7cfc4dd8d1865f4570ed28df04aee1
Latteflo <sf.simion.f@gmail.com>
2022-12-23 01:46:21 +0100

    added resume and certificates buttons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import WordCloud from '../WordCloud'
+import { Link } from 'react-router-dom'
 //import { motion } from "framer-motion"
 
 //export const MyComponent = ({ isVisible }) => (
@@ -22,41 +23,51 @@ const About = () => {
   return (
     <>
       <div className="about-page">
-         <div className="img"></div>
-         <div className="text-flex-about">
-        <div className="text-zone">
-          <h1>
-            <span className={`${letterClass} _14 special`}>A</span>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['b', 'o', 'u', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
-          </h1>
-          <br />
-          <p>
-            {' '}
-            I am an ambitious front end developer based in Belgium, currently
-            looking for a role in an IT company with the opportunity to work
-            with the latest technologies on diverse projects.{' '}
-          </p>
-          <p>
-            I'm quietly confident, naturally curious and perpetually working on
-            improving my skills.
-          </p>
-          <p>
-            With a background in aviation, my experience has given me the
-            ability to problem solve and think outside the box, which has come
-            in handy in my career as a developer. I have also found that my
-            attention to detail is an asset when working on web projects.
-          </p>
-          <p>
-            I'm a fast learner and I'm always looking for new challenges and
-            opportunities to grow as a developer.
-          </p>
-        </div>
-        {/*<Skills/>*/}
-        <WordCloud />
+        <div className="img"></div>
+        <div className="text-flex-about">
+          <div className="text-zone">
+            <h1>
+              <span className={`${letterClass} _14 special`}>A</span>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['b', 'o', 'u', 't', ' ', 'm', 'e']}
+                idx={15}
+              />
+            </h1>
+            <br />
+            <p>
+              {' '}
+              I am an ambitious front end developer based in Belgium, currently
+              looking for a role in an IT company with the opportunity to work
+              with the latest technologies on diverse projects.{' '}
+            </p>
+            <p>
+              I'm quietly confident, naturally curious and a team-worker who is
+              perpetually working on improving my skills.
+            </p>
+            <p>
+              With a background in aviation, my experience has given me the
+              ability to problem solve and think outside the box, which has come
+              in handy in my career as a developer. I have also found that my
+              attention to detail is an asset when working on web projects.
+            </p>
+            <p>
+              I'm a fast learner and I'm always looking for new challenges and
+              opportunities to grow as a developer.
+            </p>
+          </div>
+          <div className="bottom-page">
+            <div className="about-more-details">
+              <Link to="/About/Resume" className="resume-btn">
+                My Resume
+              </Link>
+              <Link to="/About/Certificates" className="certificates-btn">
+                My Certificates
+              </Link>
+            </div>
+            {/*<Skills/>*/}
+            <WordCloud />
+          </div>
         </div>
       </div>
     </>
